6fbf610d5fa00596c426999e8254893289398b49
rodrigohenriques71 <rodrigohenriques71@hotmail.com>
2021-01-31 01:02:39 -0300

    Spring Boot Rodrigo API

---
+++ rodrigocrm-api/src/main/java/com/rodrigo/crm/RodrigocrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodrigocrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodrigocrmApiApplication.class, args);
+	}
+
+}
---
+++ rodrigocrm-api/src/main/java/com/rodrigo/crm/repository/AlunoRepository.java
@@ -0,0 +1,11 @@
+package com.rodrigo.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rodrigo.crm.model.Aluno;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+}
